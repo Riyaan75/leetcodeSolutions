75c10d246d825fb3adc520b1b6f3e20c05e59234
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-06-13 16:43:28 +0530

    Time: 1 ms (99.96%), Space: 42.3 MB (94.41%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,21 +1,40 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int i,j,c=0;
-        int a[]=new int[nums.length];
-        for(i=0;i<nums.length;i++){
-            int key=nums[i];
-            for(j=0;j<nums.length;j++){
-                if(i!=j)
-                {
-                    if(nums[j]<key)
-                        c++;
-                }
-            }
-            a[i]=c;
-            c=0;
-        }
-       System.arraycopy(a,0,nums,0,nums.length);
-        return nums;
+        //Brute Force
+       //   int i,j,c=0;
+       //  int a[]=new int[nums.length];
+       //  for(i=0;i<nums.length;i++){
+       //      int key=nums[i];
+       //      for(j=0;j<nums.length;j++){
+       //          if(i!=j)
+       //          {
+       //              if(nums[j]<key)
+       //                  c++;
+       //          }
+       //      }
+       //      a[i]=c;
+       //      c=0;
+       //  }
+       // System.arraycopy(a,0,nums,0,nums.length);
+       //  return nums;
         
+        
+         int[] count = new int[101];
+        int res[]=new int[nums.length];
+        for(int i : nums){
+            count[i]++;
+        }
+        int p = 0;
+        int[] prefix = new int[101];
+        for(int i=0;i<count.length;i++){
+            prefix[i]=p;
+            p+=count[i];
+        }
+         for(int i = 0; i<nums.length; i++){
+            res[i] = prefix[nums[i]];
+        }
+        
+        
+        return res;
     }
 }

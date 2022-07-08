84842ebc28123f061a9d3b5f6674548b0acf4784
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-07-08 17:38:24 +0530

    Time: 1 ms (43.90%), Space: 45.8 MB (58.81%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -10,16 +10,30 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        ListNode t=head;
-        HashMap<ListNode,Boolean> visited=new HashMap<>();
-        while(t!=null){
-            ListNode ref=t;
-            if(visited.get(ref)!=null)
-                return true;
-            visited.put(ref,true);
-            t=t.next;
-        }
-        return false;
-    }
+      public boolean hasCycle(ListNode head) {
+          ListNode slow,fast;
+          slow=fast=head;
+          if(head==null||head.next==null)
+              return false;
+          while(slow!=null&fast!=null&&fast.next!=null){
+              slow=slow.next;
+              fast=fast.next.next;
+              if(slow==fast)
+                  return true;
+          }
+          return false;
+      }
+    
+//     public boolean hasCycle(ListNode head) {
+//         ListNode t=head;
+//         HashMap<ListNode,Boolean> visited=new HashMap<>();
+//         while(t!=null){
+//             ListNode ref=t;
+//             if(visited.get(ref)!=null)
+//                 return true;
+//             visited.put(ref,true);
+//             t=t.next;
+//         }
+//         return false;
+//     }
 }

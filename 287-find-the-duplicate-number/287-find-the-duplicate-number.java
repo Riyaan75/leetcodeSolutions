86fa18e4f701a341b2b49bc32756051871356efb
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-07-09 00:06:55 +0530

    Time: 5 ms (85.16%), Space: 74.8 MB (81.07%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,12 +1,31 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                return nums[i];
-            }
-            map.put(nums[i],1);
+    // Approach 1 hash map
+    // public int findDuplicate(int[] nums) {
+    //     HashMap<Integer,Integer> map=new HashMap<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         if(map.containsKey(nums[i])){
+    //             return nums[i];
+    //         }
+    //         map.put(nums[i],1);
+    //     }
+    //     return -1;
+    // }
+    
+    //Appraoch 2 floyd algo of fast and slow
+       public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        fast = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
     }
 }

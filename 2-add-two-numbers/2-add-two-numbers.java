c838f21be681c30d0cd12518f6bac82e2c07d78c
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-07-13 17:30:47 +0530

    Time: 4 ms (36.76%), Space: 47.5 MB (72.73%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -33,37 +33,35 @@ class Solution {
     //     return d.next;
     // }
     
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
-    {
-        ListNode head= new ListNode(-1);
-		//creating a node in order to store the sum of digit in a List format 
-        ListNode temp= head; //we are using temp to add node to the new List 
+      public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        int carry= 0; //we are storing the carry at every digit pair iteration 
+        var result = new ListNode();
+        var plus = false;
+        var cur = result;
         
-        while(l1 != null || l2 != null || carry > 0 )
-		//our loop terminating condtition, all the numbers will be added 
-        {
-            int sum= 0; //to store the sum at every digit pair iteration 
+        while (l1 != null || l2 != null || plus) {
             
-            if(l1 != null) //adding list one node to the local sum
-            {
-                sum+= l1.val; //adding to the sum 
-                l1= l1.next; //moving the l1 pointer to the next node
+            if (l1 != null) {
+                cur.val += l1.val;
+                l1 = l1.next;
             }
-            
-            if(l2 != null)
-            {
-                sum+= l2.val; //adding to the sum 
-                l2= l2.next;  //moving the l2 pointer to the next node
+            if (l2 != null) {
+                cur.val += l2.val;
+                l2 = l2.next;
             }
-            sum+= carry; //adding the sum inclusive carry
-            carry= sum / 10; //extracting the first digit 
-            ListNode node= new ListNode(sum % 10); //extracting the last digit and creating the node 
-            temp.next= node;//linking the node to the list 
-            
-            temp= temp.next;//moving the temp 
+            if (plus) {
+                ++cur.val;
+                plus = false;
+            }
+            if (cur.val >= 10) {
+                cur.val -= 10;
+                plus = true;
+            }
+            if (l1 != null || l2 != null || plus) {
+                cur.next = new ListNode();
+                cur = cur.next;
+            }
         }
-        return head.next; //reaturning the new head of the new list 
+        return result;
     }
 }

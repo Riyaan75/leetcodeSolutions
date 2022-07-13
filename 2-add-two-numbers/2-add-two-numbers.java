cfd24eb1b4df9afe0fa6afa551aa054751f9d4d5
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-07-13 23:25:40 +0530

    Time: 4 ms (36.76%), Space: 48.5 MB (12.54%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,72 +10,82 @@
  */
 class Solution {
     //Approach 1 using demo linkded list and carry
-    // public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    //     ListNode d=new ListNode();
-    //     ListNode t=d;
-    //     int car=0;
-    //     while(l1!=null || l2!=null || car==1){
-    //         int sum=0;
-    //         if(l1!=null){
-    //             sum+=l1.val;
-    //             l1=l1.next;
-    //         }
-    //         if(l2!=null){
-    //             sum+=l2.val;
-    //             l2=l2.next;
-    //         }
-    //         sum+=car;
-    //         car=sum/10; 
-    //         ListNode node=new ListNode(sum%10);
-    //         t.next=node;
-    //         t=t.next;
-    //     }
-    //     return d.next;
-    // }
-    
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode l2_head = l2, l2_end = l2;
-        int carry = 0;
-        while(l1!=null && l2!=null){
-            l2.val = l2.val + l1.val + carry;
-            if(l2.val>=10){
-                carry = 1;
-                l2.val-=10;
-            }
-            else {
-                carry = 0;
-            }
-            l2_end = l2;
-            l2 = l2.next;
-            l1 = l1.next;
-        }
-        while(l1!=null){
-            l1.val = l1.val + carry;
-            if(l1.val>=10){
-                carry = 1;
-                l1.val-=10;
-            }
-            else {
-                carry = 0;
-            }
-            l2_end.next = l1;
-            l2_end = l2_end.next;
-            l1 = l1.next;
-        }
+        ListNode d=new ListNode();
         
-        while(l2!=null){
-            l2.val = l2.val + carry;
-            if(l2.val>=10){
-                carry = 1;
-                l2.val-=10;
+        
+        ListNode t=d;
+        int car=0;
+        
+        while(l1!=null || l2!=null || car==1){
+            
+            int sum=0;
+            if(l1!=null){
+                
+                sum+=l1.val;
+                l1=l1.next;
             }
-            else {
-                carry = 0;
+            
+            if(l2!=null){
+            
+                sum+=l2.val;
+                
+                l2=l2.next;
             }
-            l2_end = l2;
-            l2 = l2.next;
+            sum+=car;
+            
+            car=sum/10; 
+            ListNode node=new ListNode(sum%10);
+            
+            t.next=node;
+            t=t.next;
         }
-        if(carry>0) l2_end.next = new ListNode(1);
-        return l2_head;
+        return d.next;
     }
+    
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//         ListNode l2_head = l2, l2_end = l2;
+//         int carry = 0;
+//         while(l1!=null && l2!=null){
+//             l2.val = l2.val + l1.val + carry;
+//             if(l2.val>=10){
+//                 carry = 1;
+//                 l2.val-=10;
+//             }
+//             else {
+//                 carry = 0;
+//             }
+//             l2_end = l2;
+//             l2 = l2.next;
+//             l1 = l1.next;
+//         }
+//         while(l1!=null){
+//             l1.val = l1.val + carry;
+//             if(l1.val>=10){
+//                 carry = 1;
+//                 l1.val-=10;
+//             }
+//             else {
+//                 carry = 0;
+//             }
+//             l2_end.next = l1;
+//             l2_end = l2_end.next;
+//             l1 = l1.next;
+//         }
+        
+//         while(l2!=null){
+//             l2.val = l2.val + carry;
+//             if(l2.val>=10){
+//                 carry = 1;
+//                 l2.val-=10;
+//             }
+//             else {
+//                 carry = 0;
+//             }
+//             l2_end = l2;
+//             l2 = l2.next;
+//         }
+//         if(carry>0) l2_end.next = new ListNode(1);
+//         return l2_head;
+//     }
 }

3dd36790210709beed4fc629d96baad79629f226
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-07-13 17:33:18 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (95.58%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -33,21 +33,49 @@ class Solution {
     //     return d.next;
     // }
     
-     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return addTwoNumbersRecursive(l1,l2,0);
-    }
-    
-    private ListNode addTwoNumbersRecursive(ListNode l1, ListNode l2, int carry)
-    {
-        if(l1 == null && l2 == null) {
-            if(carry != 0) return new ListNode(carry);
-            else return null;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l2_head = l2, l2_end = l2;
+        int carry = 0;
+        while(l1!=null && l2!=null){
+            l2.val = l2.val + l1.val + carry;
+            if(l2.val>=10){
+                carry = 1;
+                l2.val-=10;
+            }
+            else {
+                carry = 0;
+            }
+            l2_end = l2;
+            l2 = l2.next;
+            l1 = l1.next;
+        }
+        while(l1!=null){
+            l1.val = l1.val + carry;
+            if(l1.val>=10){
+                carry = 1;
+                l1.val-=10;
+            }
+            else {
+                carry = 0;
+            }
+            l2_end.next = l1;
+            l2_end = l2_end.next;
+            l1 = l1.next;
         }
         
-        int val1 = l1 == null ? 0 : l1.val;
-        int val2 = l2 == null ? 0 : l2.val;
-        int val = (val1 + val2 + carry) % 10;
-        int mod = (val1 + val2 + carry) / 10;
-        return new ListNode(val, addTwoNumbersRecursive(l1 == null ? null : l1.next, l2 == null ? null : l2.next, mod));
+        while(l2!=null){
+            l2.val = l2.val + carry;
+            if(l2.val>=10){
+                carry = 1;
+                l2.val-=10;
+            }
+            else {
+                carry = 0;
+            }
+            l2_end = l2;
+            l2 = l2.next;
+        }
+        if(carry>0) l2_end.next = new ListNode(1);
+        return l2_head;
     }
 }

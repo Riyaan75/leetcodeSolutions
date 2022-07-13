b7dd9e0c1f27829acfd22427de3ca53c301e551b
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-07-13 17:29:58 +0530

    Time: 5 ms (15.67%), Space: 48 MB (39.55%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -9,26 +9,61 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode d=new ListNode();
-        ListNode t=d;
-        int car=0;
-        while(l1!=null || l2!=null || car==1){
-            int sum=0;
-            if(l1!=null){
-                sum+=l1.val;
-                l1=l1.next;
+    //Approach 1 using demo linkded list and carry
+    // public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    //     ListNode d=new ListNode();
+    //     ListNode t=d;
+    //     int car=0;
+    //     while(l1!=null || l2!=null || car==1){
+    //         int sum=0;
+    //         if(l1!=null){
+    //             sum+=l1.val;
+    //             l1=l1.next;
+    //         }
+    //         if(l2!=null){
+    //             sum+=l2.val;
+    //             l2=l2.next;
+    //         }
+    //         sum+=car;
+    //         car=sum/10; 
+    //         ListNode node=new ListNode(sum%10);
+    //         t.next=node;
+    //         t=t.next;
+    //     }
+    //     return d.next;
+    // }
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        ListNode head= new ListNode(-1);
+		//creating a node in order to store the sum of digit in a List format 
+        ListNode temp= head; //we are using temp to add node to the new List 
+        
+        int carry= 0; //we are storing the carry at every digit pair iteration 
+        
+        while(l1 != null || l2 != null || carry > 0 )
+		//our loop terminating condtition, all the numbers will be added 
+        {
+            int sum= 0; //to store the sum at every digit pair iteration 
+            
+            if(l1 != null) //adding list one node to the local sum
+            {
+                sum+= l1.val; //adding to the sum 
+                l1= l1.next; //moving the l1 pointer to the next node
             }
-            if(l2!=null){
-                sum+=l2.val;
-                l2=l2.next;
+            
+            if(l2 != null)
+            {
+                sum+= l2.val; //adding to the sum 
+                l2= l2.next;  //moving the l2 pointer to the next node
             }
-            sum+=car;
-            car=sum/10; 
-            ListNode node=new ListNode(sum%10);
-            t.next=node;
-            t=t.next;
+            sum+= carry; //adding the sum inclusive carry
+            carry= sum / 10; //extracting the first digit 
+            ListNode node= new ListNode(sum % 10); //extracting the last digit and creating the node 
+            temp.next= node;//linking the node to the list 
+            
+            temp= temp.next;//moving the temp 
         }
-        return d.next;
+        return head.next; //reaturning the new head of the new list 
     }
 }

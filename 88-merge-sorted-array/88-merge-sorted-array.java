d2c667063f2bacd2fd5586c9d0df607a5a4f587c
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-07-18 15:46:28 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (77.12%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,8 +1,31 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=0;i<nums2.length;i++){
-            nums1[i+m]=nums2[i];
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         for(int i=0;i<nums2.length;i++){
+//             nums1[i+m]=nums2[i];
+//         }
+//         Arrays.sort(nums1);
+//     }
+    
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+  {
+   while(m>0 && n>0)
+   {
+        if(nums1[m-1]>nums2[n-1])
+		{
+            nums1[m+n-1]=nums1[m-1];
+            m--;
         }
-        Arrays.sort(nums1);
+        else
+		{
+            nums1[m+n-1]=nums2[n-1];
+            n--;
+        }
     }
+	
+    while(n>0)
+	{
+        nums1[m+n-1]=nums2[n-1];
+        n--;
+    }
+  }
 }

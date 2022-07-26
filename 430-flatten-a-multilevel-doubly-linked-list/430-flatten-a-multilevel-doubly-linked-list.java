cf17b1e36638c0d718da22837b1ffbb5ac177ba3
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-07-26 16:23:27 +0530

    Time: 1 ms (32.67%), Space: 40.5 MB (86.48%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
@@ -9,32 +9,30 @@ class Node {
 */
 
 class Solution {
-    static void merge(Node h1, Node t2)
-    {
-        t2.next=h1.next;
-        if(h1.next!=null) h1.next.prev=t2;
-        h1.next=h1.child;
-        h1.child.prev=h1;
-        h1.child=null;
+    public Node flatten(Node head) {
+        traverse(head,head);
+        return head;
     }
-    static void dft(Node h1, Node h2)
-    {
+    static void traverse(Node h1,Node h2){
         Node t=h2;
-        while(t!=null)
-        {
-            if(t.child!=null) dft(t, t.child);
-            if(t.next==null)
-            {
-                if(h1==h2) return;
-                merge(h1, t);
+        while(t!=null){
+            if(t.child!=null)
+                traverse(t,t.child);
+            if(t.next==null){
+                if(h1==h2)
+                    return;
+                merge(h1,t);
                 return;
-            }        
+            }
             t=t.next;
         }
     }
-    public Node flatten(Node head)
-    {
-        dft(head, head);
-        return head;
+    static void merge(Node h1,Node t){
+        t.next=h1.next;
+        if(h1.next!=null)
+            h1.next.prev=t;
+        h1.next=h1.child;
+        h1.next.prev=h1;
+        h1.child=null;
     }
 }

726197df7d872c52af1dbbcbca5fbd7708bb5a6f
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-08-26 17:05:46 +0530

    Time: 25 ms (25.63%), Space: 56.6 MB (5.02%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,27 +1,57 @@
 class Solution {
-    //approach 1
-    public int[] topKFrequent(int[] nums, int k) {
-        if (nums.length == k)
-            return nums;
+    //bro's approach
+     public int[] topKFrequent(int[] nums, int k) {
         
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0)+1);
+        HashSet<Integer> count[]=new HashSet [nums.length+1];
+        for(int i=0; i<count.length; i++) count[i]=new HashSet<Integer>();
+        for(int n: nums) count[0].add(n);
+        
+        HashMap <Integer, Integer> f=new HashMap<>();
+        for(int i=0; i<nums.length; i++)
+        {
+            int prevf=f.getOrDefault(nums[i], 0);
+            f.put(nums[i], prevf+1);
+            count[prevf+1].add(nums[i]);
+            count[prevf].remove(nums[i]);
         }
         
-        Queue<Integer> q = new PriorityQueue<>(
-            (n1, n2) -> map.get(n2) - map.get(n1)
-        );
-        for (int key : map.keySet()) {
-            q.add(key);
-        }
-        
-        int[] res = new int[k];
-        for (int i = 0; i < k; i++) {
-            res[i] = q.poll();
+        int res[]=new int[k];
+        int j=0;
+        for(int i=count.length-1; i>=0; i--)
+        {
+            HashSet<Integer> set=count[i];
+            if(set.size()==0) continue;
+            for(int e: set)
+            {
+                if(j==k) return res;
+                res[j++]=e;
+            }
         }
         return res;
     }
+    //approach 1
+//     public int[] topKFrequent(int[] nums, int k) {
+//         if (nums.length == k)
+//             return nums;
+        
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int n : nums) {
+//             map.put(n, map.getOrDefault(n, 0)+1);
+//         }
+        
+//         Queue<Integer> q = new PriorityQueue<>(
+//             (n1, n2) -> map.get(n2) - map.get(n1)
+//         );
+//         for (int key : map.keySet()) {
+//             q.add(key);
+//         }
+        
+//         int[] res = new int[k];
+//         for (int i = 0; i < k; i++) {
+//             res[i] = q.poll();
+//         }
+//         return res;
+//     }
     // approach 2
 //     public int[] topKFrequent(int[] nums, int k) {
 //         Map<Integer,Integer>numFreqMap=new HashMap<>();

8a149766614bb7421ecd4143149dbb1ec705c098
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-09-14 16:31:32 +0530

    Time: 1 ms (99.53%), Space: 44.9 MB (97.26%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,3 +1,26 @@
+//Two-Pointer Approach :
+class Solution {
+    
+    //  Time Complexity = O(N)
+    //  Space Complexity = O(1)
+    
+    public int[] twoSum(int[] numbers, int target) {
+        
+        //  Left and right pointer
+        int left = 0, right = numbers.length-1;
+        
+        while(numbers[left] + numbers[right] != target && left < right){
+            
+            if (numbers[left] + numbers[right] > target) 
+                right--;
+            else 
+                left++;
+        }
+        
+        return new int[]{++left, ++right};
+    }
+}
+/* HashMap approach
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int ans[] = new int[2];
@@ -13,3 +36,4 @@ class Solution {
         return ans;
     }
 }
+*/

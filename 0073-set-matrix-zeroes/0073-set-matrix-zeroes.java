dcbe30789c1f78364418de365f97975a839b034c
Riyaan75 <85544636+Riyaan75@users.noreply.github.com>
2022-10-30 11:51:55 +0530

    Time: 1 ms (97.47%), Space: 53.9 MB (63.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,23 +1,43 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int i,j;
-        int ans[][]=new int[matrix.length][matrix[0].length];
-        for(i=0;i<matrix.length;i++)
-        ans[i]=Arrays.copyOf(matrix[i],matrix[i].length);
-        for(i=0;i<matrix.length;i++){
-            for(j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0)
-                {
-                      for(int k=0;k<matrix[0].length;k++)
-                        ans[i][k]=0;
+   public void setZeroes(int[][] matrix){
 
-                    for(int k=0;k<matrix.length;k++)
-                        ans[k][j]=0;
+		int m=matrix.length, n=matrix[0].length;
+        boolean isRow0=false, isCol0=false;
+        
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0)
+                isRow0=true;
+        }
+        
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0)
+                isCol0=true;
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(i=0;i<matrix.length;i++)
-        matrix[i]=Arrays.copyOf(ans[i],ans[i].length);
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0)
+                    matrix[i][j]=0;
+            }
+        }
+        
+        if(isRow0){
+            for(int j=0;j<n;j++)
+                matrix[0][j]=0;
+        }
+        
+        if(isCol0){
+            for(int i=0;i<m;i++)
+                matrix[i][0]=0;
+        }
     }
-  
 }
